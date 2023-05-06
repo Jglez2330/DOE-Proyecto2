99a43b215eb6dbb588a323918215907283ee71d2
Joseph González <josegonzalez2330@gmail.com>
2023-05-06 17:29:08 -0600

    Add scenes to python experiment script

--- scripts/random_experiments_generator.py
+++ scripts/random_experiments_generator.py
@@ -11,11 +11,11 @@ def generate_random_exp(scenes, accelerators, integrators):
     # Writing result
     f = open("experiments.txt", "w")
     for run in runs:
-        f.write(f"{run[0]}_{run[1]}_{run[2]} \n")
+        f.write(f"{run[0]}_{run[1]}_{run[2]}.pbrt \n")
     f.close()
 
 def main():
-    scenes = ['Scene 1', 'Scene 2', 'Scene 3', 'Scene 4']
+    scenes = ['book', 'Dragon', 'killroos', 'smoke']
     accelerators = ['bvh', 'kdtree']
     integrators = ['path', 'volpath']
     generate_random_exp(scenes, accelerators, integrators)
